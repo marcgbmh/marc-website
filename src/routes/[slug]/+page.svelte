70f270321e1244c004d3313f8040afad9055f405
marc <91703789+marcgbmh@users.noreply.github.com>
2024-07-23 11:04:30 +0200

    fix: markdown line breaks

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -8,12 +8,12 @@
     <meta name="description" content={data.meta.description} />
     <meta property="og:title" content={data.meta.title} />
     <meta property="og:description" content={data.meta.description} />
-    <meta property="og:image" content={data.meta.image} />
+    <meta property="og:image" content={data.meta.thumbnail} />
     <meta property="og:url" content={data.meta.url} />
-    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:card" content={data.meta.thumbnail} />
     <meta name="twitter:title" content={data.meta.title} />
     <meta name="twitter:description" content={data.meta.description} />
-    <meta name="twitter:image" content={data.meta.image} />
+    <meta name="twitter:image" content={data.meta.thumbnail} />
   {/if}
 </svelte:head>
 
@@ -23,7 +23,7 @@
       <h1 class="text-lg mb-3 font-bold">{data.meta.title}</h1>
     {/if}
   </hgroup>
-  <div class="whitespace-normal">
+  <div class="whitespace-normal markdown">
     <svelte:component this={data.content} />
   </div>
 </article>
